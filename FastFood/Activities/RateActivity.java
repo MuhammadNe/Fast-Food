d346e0ec81b106deafad3c5bfac50c435954e5de
Muhammad Neiroukh <muhammad.ne1@gmail.com>
2015-05-15 19:47:30 +0300

    Create RateActivity.java

---
+++ FastFood/Activities/RateActivity.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ * Rating Activity
+ * 
+ */
+
+package com.MOS.fastfood;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+
+public class RateActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_rate);
+	}
+
+	
+}
