bca1efa554ed8467c7382b6af695cca288c5d57d
Sultan <sultan.kh30@gmail.com>
2015-05-20 04:17:22 +0300

    Handlerequest.py file updated

--- ServerSide/HandleRequest.py
+++ ServerSide/HandleRequest.py
@@ -9,13 +9,13 @@ def upload(request):
         return HttpResponse('Invalid Data')""""
 		
 	def handle(request):
-	host = ''        # Symbolic name meaning all available interfaces
-	port = 12345     # Arbitrary non-privileged port
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind((host, port))
-	s.listen(1)
-	conn, addr = s.accept()
-	print('Connected by', addr)
+		host = ''        # Symbolic name meaning all available interfaces
+		port = 12345     # Arbitrary non-privileged port
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.bind((host, port))
+		s.listen(1)
+		conn, addr = s.accept()
+		print('Connected by', addr)
 	while True:
 		data = conn.recv(1024)
 		if not data: break
