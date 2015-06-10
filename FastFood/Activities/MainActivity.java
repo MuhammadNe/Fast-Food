56afd08097bc0b7c3d3afc5ed8b09191c6445ed9
Muhammad Neiroukh <muhammad.ne1@gmail.com>
2015-06-10 15:33:49 +0300

    Update MainActivity.java

--- FastFood/Activities/MainActivity.java
+++ FastFood/Activities/MainActivity.java
@@ -8,22 +8,8 @@
 package com.MOS.fastfood;
 
 
-
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -35,16 +21,26 @@ import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
-
-	Button list,get;
-	String URL = "http://jce-fastfood-project.appspot.com";
-	String tempResult ="";
 	
+	//******** Defining Objects
 	RestaurantData restaurantData = new RestaurantData();
+
+	//******** Defining String
+	String URL = "http://jce-fastfood-project.appspot.com/?togo=restfunctions";
+	String tempResult ="";
+
+	//******** Defining Buttons
+	Button list,get;
+	
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		HttpGetConnection getRequest = new HttpGetConnection();
+		getRequest.execute(URL);
+		
 		list = (Button) findViewById(R.id.save);
 		get = (Button) findViewById(R.id.get);
 		final TextView tv = (TextView) findViewById(R.id.showData);
@@ -55,15 +51,15 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				
-				new RequestTask().execute("http://jce-fastfood-project.appspot.com/?togo=restfunctions");
 				
-				//tv.append(RestaurantData.Data.size()+"");
-				
-				/*for(int i =0; i < RestaurantData.Data.size(); i++)
+				//Toast.makeText(getApplicationContext(), HttpGetConnection.tempResult, Toast.LENGTH_LONG).show();
+				//System.out.println(HttpGetConnection.tempResult);
+				RestaurantData restaurantData = new RestaurantData();
+				System.out.println(RestaurantData.rate.size() + "-" + RestaurantData.name.size());
+				for(int i =0; i < RestaurantData.rate.size(); i++)
 				{
-					tv.append(RestaurantData.Data.get(i) + "--\n");
-
-				}*/
+					tv.append(restaurantData.rate.get(i) + "--\n");
+				}
 				//tv.append(RestaurantData.lng.get(RestaurantData.lng.size()));			
 			}
 		});
@@ -89,50 +85,4 @@ public class MainActivity extends Activity {
 		});
 	}
 	
-	/***********************************Sending HTTP GET REQUES****************************************************/
-
-	
-	
-	class RequestTask extends AsyncTask<String, String, String>{
-
-		@Override
-		protected String doInBackground(String... uri) {
-			HttpClient httpclient = new DefaultHttpClient();
-			HttpResponse response;
-			String responseString = null;
-			try {
-				response = httpclient.execute(new HttpGet(uri[0]));
-				StatusLine statusLine = response.getStatusLine();
-				if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-					ByteArrayOutputStream out = new ByteArrayOutputStream();
-					response.getEntity().writeTo(out);
-					responseString = out.toString();
-					out.close();
-				} else{
-					//Closes the connection.
-					response.getEntity().getContent().close();
-					throw new IOException(statusLine.getReasonPhrase());
-				}
-			} catch (ClientProtocolException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return responseString;
-		}
-
-		@Override
-		protected void onPostExecute(String result) {
-			super.onPostExecute(result);
-			tempResult = result;
-			//Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
-			//restaurantData.parse(result);
-			//RestaurantData.Data.add(result);
-			Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
-		}
-	}
-	
-
-
 }
-
